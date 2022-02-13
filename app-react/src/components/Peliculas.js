d5127b5e5a78d3daff583457d885395a99a4cbfc
Gerarck2021 <gaarciniegas23@misena.edu.co>
2022-02-13 17:52:19 -0500

    Se añaden las props en generos y valores al componente peliculas en TiraPeliculas.js

--- app-react/src/components/Peliculas.js
+++ app-react/src/components/Peliculas.js
@@ -3,14 +3,15 @@ import React from 'react';
 //componente que tiene titulo y rating
 function Pelicula(props) {
     
-    let generos = ["Accion", "Drama"];
     return(
         <div>
+            {/*Se colocan las props en titulo y rating para agregar datos en otros componentes*/}
             <h2 className="TituloPeli">{ props.titulo }</h2>
             <p>Rating de Pelicula: { props.rating }</p>
             <h3>Generos de la Pelicula</h3>
             <ul>
-                { generos.map( genero => <li>{ genero}</li>)}
+                {/*Se coloca props en generos y una key para que no tenga problemas de renderizar el <li>*/}
+                { props.generos.map( (genero, i) => <li key={i + "movie2022"}>{ genero}</li>)}
             </ul>
         </div>
     )
--- app-react/src/components/TiraPeliculas.js
+++ app-react/src/components/TiraPeliculas.js
@@ -4,11 +4,12 @@ import Pelicula from '../components/Peliculas';
 function TiraPeliculas() {
     return (
         <div>
-            <Pelicula rating="8.8" titulo="Toy Story"/>
-            <Pelicula rating="6.8" titulo="Malefica"/>
-            <Pelicula rating="5" titulo="Spider Man"/>
-            <Pelicula rating="7.8" titulo="Alicia en el pais de las Maravillas"/>
-            <Pelicula rating="9" titulo="Encanto"/>
+            {/*Se usan 5 veces el componente de pelicula y se le añaden valores a las propiedades*/}
+            <Pelicula generos = { ["Aventura", "Animacion"]} rating="8.8" titulo="Toy Story"/>
+            <Pelicula generos = { ["Aventura", "Animacion"]} rating="6.8" titulo="Malefica"/>
+            <Pelicula generos = { ["Aventura", "Accion"]} rating="5" titulo="Spider Man"/>
+            <Pelicula generos = { ["Aventura", "Animacion", "Accion"]}rating="7.8" titulo="Alicia en el pais de las Maravillas"/>
+            <Pelicula generos = { ["Comedia", "Animacion"]}rating="9" titulo="Encanto"/>
         </div>
     )
 }
