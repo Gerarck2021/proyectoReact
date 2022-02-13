1416b92c1cea0680d657af987be5a86abf7f6fe5
Gerarck2021 <gaarciniegas23@misena.edu.co>
2022-02-13 17:32:11 -0500

    Se añaden las props a la funcion de Peliculas y se añaden variables al componente de TiraPeliculas.js

--- app-react/src/components/Peliculas.js
+++ app-react/src/components/Peliculas.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 //componente que tiene titulo y rating
-function Pelicula() {
-    let rating = 5.8;
+function Pelicula(props) {
+    
     let generos = ["Accion", "Drama"];
     return(
         <div>
-            <h2 className="TituloPeli">Titulo de Pelicula</h2>
-            <p>Rating de Pelicula: { rating }</p>
+            <h2 className="TituloPeli">{ props.titulo }</h2>
+            <p>Rating de Pelicula: { props.rating }</p>
             <h3>Generos de la Pelicula</h3>
             <ul>
                 { generos.map( genero => <li>{ genero}</li>)}
--- app-react/src/components/TiraPeliculas.js
+++ app-react/src/components/TiraPeliculas.js
@@ -4,11 +4,11 @@ import Pelicula from '../components/Peliculas';
 function TiraPeliculas() {
     return (
         <div>
-            <Pelicula />
-            <Pelicula />
-            <Pelicula />
-            <Pelicula />
-            <Pelicula />
+            <Pelicula rating="8.8" titulo="Toy Story"/>
+            <Pelicula rating="6.8" titulo="Malefica"/>
+            <Pelicula rating="5" titulo="Spider Man"/>
+            <Pelicula rating="7.8" titulo="Alicia en el pais de las Maravillas"/>
+            <Pelicula rating="9" titulo="Encanto"/>
         </div>
     )
 }
