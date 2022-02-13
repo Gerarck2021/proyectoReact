cbaeff0620d357f39d8f2ddec4d3f6c3b6d75789
Gerarck2021 <gaarciniegas23@misena.edu.co>
2022-02-13 16:22:21 -0500

    Se crea el componente Saludo.js

---
+++ app-react/src/Saludo.js
@@ -0,0 +1,11 @@
+// 1. al crear un nuevo componente, importamos React
+import React from 'react';
+
+// 2. creamos la funcion
+function Saludo() {
+    return (
+        <h1>Hola Mundo</h1>
+    )
+}
+
+// 3. exportamos la funcion
