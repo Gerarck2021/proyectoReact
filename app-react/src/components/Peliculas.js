ee7c6eb1afa71f0152d54a882e4fa83b12ecc735
Gerarck2021 <gaarciniegas23@misena.edu.co>
2022-02-13 23:08:04 -0500

    Se instala prop-types, se hacen los defaultProps en Peliculas.js y se hace validacion de generos en el mismo

--- app-react/src/components/Peliculas.js
+++ app-react/src/components/Peliculas.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+//depsues de instalado, importamos el prop types
+import propTypes from 'prop-types';
 //componente que tiene titulo y rating
 function Pelicula(props) {
     
+    let listaGeneros;
+    if(props.generos != null ) {
+        listaGeneros = 
+            <ul>
+                {/*Se coloca props en generos y una key para que no tenga problemas de renderizar el <li>*/}
+                { props.generos.map( (genero, i) => <li key={i + "movie2022"}>{ genero}</li>)}
+            </ul>
+    } else {
+        listaGeneros = "";
+    }
     return(
+        
         <div>
             {/*Se colocan las props en titulo y rating para agregar datos en otros componentes*/}
             <h2 className="TituloPeli">{ props.titulo }</h2>
@@ -11,10 +24,15 @@ function Pelicula(props) {
             <h3>Generos de la Pelicula</h3>
             <ul>
                 {/*Se coloca props en generos y una key para que no tenga problemas de renderizar el <li>*/}
-                { props.generos.map( (genero, i) => <li key={i + "movie2022"}>{ genero}</li>)}
+                { listaGeneros }
             </ul>
         </div>
     )
+}
+
+Pelicula.defaultProps = {
+    rating: "Esta pelicula aun no ha sido puntuada",
+    generos: null
 }
 
 export default Pelicula;
