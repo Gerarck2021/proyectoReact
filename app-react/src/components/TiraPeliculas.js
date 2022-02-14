e6a324513383ad0d4a90c9b163e37393b5ac700b
Gerarck2021 <gaarciniegas23@misena.edu.co>
2022-02-13 23:23:43 -0500

    Correcion en el rating de TiraPeliculas.js

--- app-react/src/components/TiraPeliculas.js
+++ app-react/src/components/TiraPeliculas.js
@@ -5,11 +5,11 @@ function TiraPeliculas() {
     return (
         <div>
             {/*Se usan 5 veces el componente de pelicula y se le añaden valores a las propiedades*/}
-            <Pelicula generos = { ["Aventura", "Animacion"]} rating="8.8" titulo="Toy Story"/>
-            <Pelicula generos = { ["Aventura", "Animacion"]} rating="6.8" titulo="Malefica"/>
-            <Pelicula generos = { ["Aventura", "Accion"]} rating="5" titulo="Spider Man"/>
-            <Pelicula generos = { ["Aventura", "Animacion", "Accion"]}rating="7.8" titulo="Alicia en el pais de las Maravillas"/>
-            <Pelicula generos = { ["Comedia", "Animacion"]}rating="9" titulo="Encanto"/>
+            <Pelicula generos = { ["Aventura", "Animacion"]} rating={8.8} titulo="Toy Story"/>
+            <Pelicula generos = { ["Aventura", "Animacion"]} rating={6.8} titulo="Malefica"/>
+            <Pelicula generos = { ["Aventura", "Accion"]} rating={5} titulo="Spider Man"/>
+            <Pelicula generos = { ["Aventura", "Animacion", "Accion"]}rating={7.8} titulo="Alicia en el pais de las Maravillas"/>
+            <Pelicula generos = { ["Comedia", "Animacion"]}rating={9} titulo="Encanto"/>
         </div>
     )
 }
